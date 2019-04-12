77b42a58956a9027ef122854ac8c07e0c9d5934d
Marta Zażlak <marta.zazlak@hotmail.com>
2019-04-12 08:14:52 +0200

    initial commit - add folders, structures

---
+++ src/models/MorseMessage.js
@@ -0,0 +1,8 @@
+class MorseMessage {
+    constructor(textMessage = '', morseTextMessage = '') {
+        this.textMessage = textMessage;
+        this.morseTextMessage = morseTextMessage;
+    }
+}
+
+export default MorseMessage;
